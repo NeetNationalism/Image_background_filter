27c8eb19ec8927d611e247ab65e666edc31a1008
NeetNationalism <neetnatioanalism@gmail.com>
2019-01-08 16:25:08 +0200

    Added a fully working version

--- image.py
+++ image.py
@@ -7,10 +7,20 @@ im_array=np.asarray(im)
 arr_shape=im_array.shape
 HEIGHT=arr_shape[0]
 WIDTH=arr_shape[1]
-print(im_array.dtype)
 arr_filtered=np.zeros((HEIGHT,WIDTH,4),np.uint8)
+
+def is_not_color(x,c=[255,255,255]):
+    #return True if x color is NOT he same as c 
+    #else return False
+    #default is white
+    value=0
+    for i in range(3):
+        value+=c[i]-x[i]
+    return value>150
+ 
 for i in range(HEIGHT):
     for j in range(WIDTH):
-        if np.array_equal(im_array[i,j],[255,255,255]):
-            arr_filtered[i,j]=[255,0,0,255]
+        if is_not_color(im_array[i,j]):
+            arr_filtered[i,j,:3]=im_array[i,j]
+            arr_filtered[i,j,3]=255
 Image.fromarray(arr_filtered).show()
